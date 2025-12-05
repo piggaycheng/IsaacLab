788fe828c56701edbdfa1705f72093657de87697
Yu Cheng <yucheng@mirle.com.tw>
2025-12-05 11:14:07 +0800

    feat: add default parameters for FourLegsPMTGActionCfg class

--- source/isaaclab/isaaclab/envs/mdp/actions/actions_cfg.py
+++ source/isaaclab/isaaclab/envs/mdp/actions/actions_cfg.py
@@ -341,6 +341,15 @@ class FourLegsPMTGActionCfg(ActionTermCfg):
         default_swing_duty_cycle: float = 0.5
         """Fixed swing duty cycle ratio. Defaults to 0.5."""
 
+        default_frequency: float = 2.0
+        """Default frequency (Hz). Defaults to 2.0."""
+        default_step_length_x: float = 0.0
+        """Default step length in the X direction (m). Defaults to 0.0."""
+        default_length_y: float = 0.0
+        """Default step length in the Y direction (m). Defaults to 0.0."""
+        default_height: float = 0.1
+        """Default step height (m). Defaults to 0.1."""
+
         # Frequency limits
         frequency_limit: tuple[float, float] = (1.0, 4.0)
         """Frequency limits (Hz). Defaults to (1.0, 4.0)."""
